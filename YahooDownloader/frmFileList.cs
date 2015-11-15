82cc578870ba11319808693e8f7360d9ac22e525
ovnisoftware <info@ovnisoftware.com>
2015-11-14 20:10:28 -0500

    Added search for .csv files

--- YahooDownloader/frmFileList.cs
+++ YahooDownloader/frmFileList.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace YahooDownloader
@@ -12,6 +14,21 @@ namespace YahooDownloader
         {
             InitializeComponent();
             folderPath = folder;
+        }
+
+        private void txtSearchBox_TextChanged(object sender, EventArgs e)
+        {
+            //clear box
+            lstFiles.Items.Clear();
+
+            //get paths and file names as list
+            List<string> allFiles = Directory.GetFiles(folderPath, "*.csv").ToList();
+
+            //search through file names, excluding paths and extensions
+            var onlyFileNames = allFiles.Where(x => Path.GetFileNameWithoutExtension(x).Contains(txtSearchBox.Text.ToUpper()));
+
+            //show reduced file set in list box
+            lstFiles.Items.AddRange(onlyFileNames.ToArray());
         }
 
         private void frmFileList_Load(object sender, EventArgs e)
