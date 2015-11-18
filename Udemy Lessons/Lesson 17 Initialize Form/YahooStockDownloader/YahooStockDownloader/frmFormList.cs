7290f07348faa2f5951347db30fc3cbda993b08d
ovnisoftware <info@ovnisoftware.com>
2015-11-18 15:31:43 -0500

    Added Lessons 15-18

---
+++ Udemy Lessons/Lesson 17 Initialize Form/YahooStockDownloader/YahooStockDownloader/frmFormList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace YahooStockDownloader
+{
+    public partial class frmFormList : Form
+    {
+        string folderPath = "";
+        public frmFormList(string folder)
+        {
+            InitializeComponent();
+            folderPath = folder;
+        }
+
+        private void frmFormList_Load(object sender, EventArgs e)
+        {
+            string[] csvFiles = Directory.GetFiles(folderPath, "*.csv");
+            lstFileList.Items.AddRange(csvFiles); 
+        }
+    }
+}
+
+
+
+
